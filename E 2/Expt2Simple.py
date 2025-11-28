edace2cbb38804e25ffd3f94347b210e123c2900
AYUSHGUPTAAA01 <149099763+AYUSHGUPTAAA01@users.noreply.github.com>
2025-11-28 10:50:25 +0530

    Update Expt2Simple.py

--- E 2/Expt2Simple.py
+++ E 2/Expt2Simple.py
@@ -1,16 +1,11 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Sep  2 10:02:29 2025
-
-@author: navee
-"""
-
 import hashlib 
 
-M="Hello"
-Digest=hashlib.sha256(M.encode()).hexdigest()
-print (M)
+M = input("Enter message: ")
+Digest = hashlib.sha256(M.encode()).hexdigest()
+print(M)
 print(Digest)
-M="Hell"
-Digest=hashlib.sha256(M.encode()).hexdigest()
+
+M = M + " "
+Digest = hashlib.sha256(M.encode()).hexdigest()
 print(Digest)
